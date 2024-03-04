3517819bb5cd9bba07467c843cbfc594fc865870
mila <milavoronina1997@mail.ru>
2024-03-04 21:35:07 +0500

    dev: create model

---
+++ src/main/java/com/voronina/gb8/model/Record.java
@@ -0,0 +1,26 @@
+package com.voronina.gb8.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Record {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "date")
+    private LocalDateTime date;
+}
